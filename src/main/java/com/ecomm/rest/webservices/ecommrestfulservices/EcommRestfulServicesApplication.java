6ac470acd9ae2c4f9a4733b308b08ee8096ee8ce
vyeluri <vyeluri5@gmail.com>
2018-06-07 16:19:05 +1000

    Initial commit

---
+++ src/main/java/com/ecomm/rest/webservices/ecommrestfulservices/EcommRestfulServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.rest.webservices.ecommrestfulservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommRestfulServicesApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EcommRestfulServicesApplication.class, args);
+	}
+}
